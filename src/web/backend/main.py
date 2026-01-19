185087d1feaa13a4bbbb1c3cb023be2521d11edd
LucasZhou <donghao.zhou@outlook.com>
2026-01-19 15:47:09 -0600

    1.3.1

--- src/web/backend/main.py
+++ src/web/backend/main.py
@@ -1,22 +1,28 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-
 from encoding.encoder import encode
 from encoding.decoder import decode
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(title="Pingu Translator API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class TextMessage(BaseModel):
     text: str
 
 
-app = FastAPI()
-
-
-@app.post("/encode")
+@app.post("/api/encode")
 def encode_text(message: TextMessage):
     return {"result": encode(message.text)}
 
 
-@app.post("/decode")
+@app.post("/api/decode")
 def decode_text(message: TextMessage):
     return {"result": decode(message.text)}
